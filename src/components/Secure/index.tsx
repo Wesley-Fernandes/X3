b5a152655caa71e8b7242261d68bb1dd2255fac5
Wesley Mesa 3 <wesleyintelligence@protonmail.com>
2023-06-04 07:49:19 -0300

    security permission fix

--- src/components/Secure/index.tsx
+++ src/components/Secure/index.tsx
@@ -1,76 +1,68 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import {usePathname, useRouter} from 'next/navigation'
+import React, { useEffect, useState } from 'react';
+import { useRouter, usePathname} from 'next/navigation';
+import { supabase } from '@modules/supabase/supabase';
+
+
 import Navbar from '../Navbar';
 import LoadingPage from '../LoadingPage';
-import { supabase } from '@modules/supabase/supabase';
-import { contextUSER } from '@modules/context/ctx_user';
 import NotAllowed from '../NotAllowed';
-import { ICreatingUser } from '@modules/app/CreateUser/page';
 
-interface ISecure{
-    children: React.ReactNode;
+interface ISecureProps {
+  children: React.ReactNode;
 }
-export default function Secure({children}:any) {
+
+export default function Secure({ children }: ISecureProps) {
   const [loading, setLoading] = useState<boolean>(true);
+  const [permission, setPermission] = useState<boolean>(false);
   const [user, setUser] = useState<any>({
-    admin:false,
-    user:false
+    admin: false,
+    user: false,
   });
+
   const {push} = useRouter();
   const pathname = usePathname();
 
-  async function checkToken(){
-    const local = localStorage.getItem('X3_account_user');
+  async function checkToken() {
+    const localstore = localStorage.getItem('X3_account_user');
 
-    if(!local){
-      console.error('Have nothing in localstore.')
-      console.log(local)
-      setLoading(false)
-      return
-    }else{
-      const user_data:any = await JSON.parse(local);
-
-      if(user_data){
-        console.log('We found something');
-        setUser(user_data[0]);
+    if(localstore){
+      const local = await JSON.parse(localstore);
+      if(local){
+        setUser(local[0]);
+        setPermission(true);
         setLoading(false)
         return
       }
-
     }
 
-    return
-  };
-
-
-  useEffect(()=>{
-    checkToken()
-  }, []);
-
-  if(pathname=='/'||pathname=='/CreateUser'){
-    return children
-  }else{
-    return(
-      <>
-
-        {loading==true  &&(<LoadingPage/>)}
-        {loading==false && user.admin==true   && user.user==false &&(
-          <>
-            <Navbar level={'Admin'} image={user?.picture}/>
-            {children}
-          </>
-        )}
-  
-        {loading==false && user.admin==false && user.user==true &&(
-          <>
-            <Navbar level={'User'} image={user?.picture}/>
-            {children}
-          </>
-        )}
-        {loading==false && user.admin==false  && user.user==false &&(<NotAllowed/>)}
-      </>
-    )
+    if(!localstore){
+      setPermission(true);
+      setLoading(false)
+      return
+    }
   }
 
+  useEffect(() => {
+    checkToken();
+  }, []);
+
+  if (pathname === '/' || pathname === '/CreateUser') {
+    return children;
+  }
+
+  if (loading && !permission) {
+    return <LoadingPage />;
+  }
+
+  if (!permission && !loading) {
+    return <NotAllowed />;
+  }
+
+  return (
+    <>
+      <Navbar level={user.admin ? 'Admin' : 'User'} image={user?.picture} />
+      {children}
+    </>
+  );
 }
